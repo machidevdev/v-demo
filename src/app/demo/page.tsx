b61dca713a4487ab8bf10480aedb2ae577000707
machidevdev <0xmachidev@gmail.com>
2025-04-07 19:06:05 +0200

    add DemoPage component with layout including Navbar, TickerBar, and DesktopView

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,22 @@
+import { ResizableHandle } from "@/components/ui/resizable";
+import { ResizablePanelGroup } from "@/components/ui/resizable";
+import { ResizablePanel } from "@/components/ui/resizable";
+import Navbar from "./components/navbar";
+import TickerBar from "./components/tickerBar";
+import OrderBook from "./components/orderbook";
+import { Menubar, MenubarItem } from "@/components/ui/menubar";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+import { Trade } from "./components/trade";
+import DesktopView from "./desktopView";
+import Positions from "./components/positions";
+export default function DemoPage() {
+  return (
+    <div className="flex min-h-screen w-full flex-col border p-2">
+      <Navbar />
+      <TickerBar />
+      <div className="w-full flex-1">
+        <DesktopView />
+      </div>
+    </div>
+  );
+}
